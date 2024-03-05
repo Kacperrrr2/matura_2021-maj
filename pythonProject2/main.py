f7b58e45fbbf8dfab6e971c49f8d3ef115058443
kacper <k4cper.petelicki@gmail.com>
2024-03-05 11:39:00 +0100

    finished exam

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -32,17 +32,48 @@ licznik=0
 #
 # print(najwiekszy+1)
 #ZADANIE 4.3
-
-najwiekszy=0
-
+# slownik={}
+# najwiekszy=0
+#
+# for wiersz in wiersze:
+#     wiersz=wiersz.strip().split()
+#     tablica.append(wiersz)
+#
+# ilosc_wystapien=0
+# znak_wystapien=""
+# for i in range(0,len(tablica)):
+#     if tablica[i][0] == "DOPISZ":
+#         klucz = tablica[i][1]
+#         ilosc_wystapien = 0
+#         for litera in klucz:
+#             if litera in slownik:
+#                 slownik[litera] += 1
+#             else:
+#                 slownik[litera] = 1
+# najwieksza=max(slownik.keys())
+# klucz_najwiekszej_wartosci = max(slownik, key=slownik.get)
+#
+# print("Klucz największej wartości:", klucz_najwiekszej_wartosci)
+#4.3
+wyraz=[]
 for wiersz in wiersze:
-    wiersz=wiersz.strip().split()
-    tablica.append(wiersz)
+     wiersz=wiersz.strip().split()
+     tablica.append(wiersz)
 
-ilosc wystapien=0
-znak_wystapien=""
 for i in range(0,len(tablica)):
-    if tablica[i][0]=="DOPISZ":
-        if tablica[i][1])
+    if tablica[i][0] == "DOPISZ":
+       wyraz.append(tablica[i][1])
+    if tablica[i][0] == "ZMIEN":
+        wyraz[len(wyraz)-1]=tablica[i][1]
+    if tablica[i][0] == "USUN":
+        wyraz = wyraz[:-1]
+    if tablica[i][0] == "PRZESUN":
+        for j in range(0,len(wyraz)):
+            if wyraz[j]==tablica[i][1]:
+                if wyraz[j]=='Z':
+                    wyraz[j]='A'
+                else:
+                    wyraz[j]=chr(ord(wyraz[j])+1)
+                    break
 
-
+print(wyraz)
